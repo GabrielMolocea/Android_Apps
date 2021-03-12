af453218a02f1c810e5eb7a90fa8e967ab4e5f97
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-03-12 19:27:24 +0200

    Adding DriverActivity

---
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/DriverLoginActivity.java
@@ -0,0 +1,97 @@
+package com.gabriel.taxifee;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * DriverLoginActivity is responsible for authentication of user or adding a new one
+ */
+public class DriverLoginActivity extends AppCompatActivity {
+
+    // Initialization of objects for Driver Login
+    private Button loginButton, registrationButton;
+    private EditText emailText, passwordText;
+
+    // Initialization of FireBase
+    private FirebaseAuth authentication;
+    private FirebaseAuth.AuthStateListener fireBaseOfListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        // Implementing FireBase authentication
+        authentication = FirebaseAuth.getInstance();
+        fireBaseOfListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(DriverLoginActivity.this,. class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        // Buttons of DriverActivity
+        loginButton = (Button) findViewById(R.id.login);
+        registrationButton = (Button) findViewById(R.id.register);
+
+        // EditText for DriverActivity
+        emailText = (EditText) findViewById(R.id.email_address);
+        passwordText = (EditText) findViewById(R.id.password);
+
+        // Adding functionality to register button
+        registrationButton.setOnClickListener(v -> {
+            final String email = emailText.getText().toString();
+            final String password = passwordText.getText().toString();
+
+            authentication.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(DriverLoginActivity.this, task -> {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(DriverLoginActivity.this, "Sign up error", Toast.LENGTH_SHORT).show();
+                        } else {
+                            String userID = authentication.getCurrentUser().getUid();
+                            DatabaseReference currentUserDB = FirebaseDatabase.
+                                    getInstance()
+                                    .getReference()
+                                    .child("Users")
+                                    .child("Driver")
+                                    .child(userID);
+                            currentUserDB.setValue(true);
+                        }
+                    });
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        authentication.addAuthStateListener(fireBaseOfListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        authentication.removeAuthStateListener(fireBaseOfListener);
+    }
+}
--- TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
@@ -1,14 +1,33 @@
 package com.gabriel.taxifee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * MainActivity is responsible for initializing of objects
+ */
 public class MainActivity extends AppCompatActivity {
+
+    // Initialization of objects
+    private Button buttonDriver, buttonCustomer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonDriver = (Button) findViewById(R.id.driver);
+        buttonCustomer = (Button) findViewById(R.id.customer);
+
+        buttonDriver.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        });
     }
 }
