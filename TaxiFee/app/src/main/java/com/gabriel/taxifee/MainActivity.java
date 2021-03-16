712046162e147af95061efe70c8213a70f2032c4
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-03-16 19:57:39 +0200

    Cleaning up some the code

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
@@ -2,6 +2,7 @@ package com.gabriel.taxifee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,14 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonDriver, buttonCustomer;
 
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideSystemUI();
 
         buttonDriver = (Button) findViewById(R.id.driver);
         buttonCustomer = (Button) findViewById(R.id.customer);
@@ -36,5 +41,19 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         });
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
