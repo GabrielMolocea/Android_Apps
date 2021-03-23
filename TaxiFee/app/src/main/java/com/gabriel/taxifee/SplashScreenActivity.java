384e0b071912f3967482affd404be97f7776fc10
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-03-23 18:32:55 +0200

    Configurating text fields

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/SplashScreenActivity.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/SplashScreenActivity.java
@@ -2,17 +2,22 @@ package com.gabriel.taxifee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -112,6 +117,22 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void showRegisterLayout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.DialogTheme);
+        View itemView = LayoutInflater.from(this).inflate(R.layout.registry_layout, null);
+
+        TextInputEditText edit_first_name = (TextInputEditText) itemView.findViewById(R.id.edit_first_name);
+        TextInputEditText edit_last_name = (TextInputEditText) itemView.findViewById(R.id.edit_last_name);
+        TextInputEditText edit_phone_number = (TextInputEditText) itemView.findViewById(R.id.edit_phone_number);
+
+        Button button_continue = (Button) itemView.findViewById(R.id.button_continue);
+
+        // Set data
+        if (FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber() != null &&
+                !TextUtils.isEmpty(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber())) {
+            edit_phone_number.setText(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+        }
+
+
     }
     private void showLoginLayout() {
         AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
