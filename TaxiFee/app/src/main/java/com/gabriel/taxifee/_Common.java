040b8a476236d0ac91b741236699410607c2a999
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-03-25 18:54:24 +0200

    Added Google Maps Activity

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/_Common.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/_Common.java
@@ -1,5 +1,10 @@
 package com.gabriel.taxifee;
 
+import com.gabriel.taxifee.model.DriverInfoModel;
+
 public class _common {
     public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
+
+    public static DriverInfoModel currentUser;
+
 }
---
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/ui/home/HomeFragment.java
@@ -0,0 +1,46 @@
+package com.gabriel.taxifee.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.gabriel.taxifee.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
