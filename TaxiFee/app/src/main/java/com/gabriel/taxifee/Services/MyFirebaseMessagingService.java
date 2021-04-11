da5fbdc0e28e671da8228a05388277903c2a3269
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-04-11 16:15:31 +0300

    Addend Firebase Cloud

---
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/Services/MyFirebaseMessagingService.java
@@ -0,0 +1,20 @@
+package com.gabriel.taxifee.Services;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+}
