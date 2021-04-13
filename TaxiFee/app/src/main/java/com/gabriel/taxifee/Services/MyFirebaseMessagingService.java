b3c0b3baeb50cd17d5ce1f04f96a67f3de5701d1
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-04-13 18:14:22 +0300

    added more on notification system

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/Services/MyFirebaseMessagingService.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/Services/MyFirebaseMessagingService.java
@@ -3,11 +3,13 @@ package com.gabriel.taxifee.Services;
 import androidx.annotation.NonNull;
 
 import com.gabriel.taxifee.Utils.UserUtils;
+import com.gabriel.taxifee._common;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 import java.util.Map;
+import java.util.Random;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -23,7 +25,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Map<String>
+        Map<String, String> dataRecv =  remoteMessage.getData();
+        if (dataRecv != null) {
+            _common.showNotification(this,
+                    new Random().nextInt(),
+                    dataRecv.get(_common.NOTI_TITLE),
+                    dataRecv.get(_common.NOTI_CONTENT),
+                    null);
+        }
     }
 
 }
--- TaxiFee/app/src/main/java/com/gabriel/taxifee/_Common.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/_Common.java
@@ -1,11 +1,18 @@
 package com.gabriel.taxifee;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
 import com.gabriel.taxifee.model.DriverInfoModel;
 
 public class _common {
     public static final String DRIVER_INFO_REFERENCE = "DriverInfo";
     public static final String DRIVER_LOCATION_REFERENCES = "DriverLocation";
     public static final String TOKEN_REFERENCE = "Token";
+    public static final String NOTI_TITLE = "title";
+    public static final String NOTI_CONTENT = "body";
 
     public static DriverInfoModel currentUser;
 
@@ -17,6 +24,17 @@ public class _common {
                     .append(_common.currentUser.getLastName()).toString();
         } else {
             return "";
+        }
+    }
+
+    public static void showNotification(Context context, int id, String title, String body, Intent intent) {
+        PendingIntent pendingIntent =  null;
+        if (intent != null) {
+            pendingIntent = PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            String NOTIFICATION_CHANNEL_ID = "taxi_fee";
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            
         }
     }
 }
