20a12eaa34110fd54061de90326e89a1ed3b9978
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-04-15 18:25:26 +0300

    Added more on notification system

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/_Common.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/_Common.java
@@ -1,9 +1,16 @@
 package com.gabriel.taxifee;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
 
 import com.gabriel.taxifee.model.DriverInfoModel;
 
@@ -34,6 +41,25 @@ public class _common {
 
             String NOTIFICATION_CHANNEL_ID = "taxi_fee";
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "TaxiFee", NotificationManager.IMPORTANCE_HIGH);
+
+                notificationChannel.setDescription("TaxiFee");
+                notificationChannel.enableLights(true);
+                notificationChannel.setLightColor(Color.RED);
+                notificationChannel.setVibrationPattern(new long[] {0, 1000, 500, 1000});
+                notificationChannel.enableVibration(true);
+
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+            builder.setContentTitle(title)
+                    .setContentText(body)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setDefaults(Notification.DEFAULT_VIBRATE)
+                    .setSmallIcon(R.drawable.ic_baseline_directions_car_24)
+                    .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_baseline_directions_car_24));
             
         }
     }
