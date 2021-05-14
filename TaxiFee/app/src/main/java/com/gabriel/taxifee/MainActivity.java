233f82d6b696441fd9392a7637f2d2f2bd6e34df
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-05-14 18:09:38 +0300

    Permissions for mapBox

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
@@ -1,13 +1,13 @@
 package com.gabriel.taxifee;
 
+import android.content.DialogInterface;
 import android.graphics.Point;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mapbox.android.core.location.LocationEngine;
@@ -15,12 +15,15 @@ import com.mapbox.android.core.permissions.PermissionsListener;
 import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
 import com.mapbox.services.android.navigation.ui.v5.location.LocationEngineConductorListener;
 import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineConductorListener,
         PermissionsListener, MapboxMap.OnMapClickListener {
@@ -45,6 +48,62 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_maps);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        map = mapboxMap;
+        enablingLocation();
+    }
+
+    @Override
+    public void onMapClick(LatLng point) {
+
+    }
+
+    // Enabling Location
+    private void enablingLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            // do stuff
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    private void initializeLocationEngine() {
+        
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Permissions")
+                .setMessage(R.string.permissions)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        //Toast.makeText(this, "Need location to determinate course", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            enablingLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull  String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onLocationUpdate(Location location) {
+
     }
 
     @Override
@@ -88,4 +147,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onDestroy();
         mapView.onDestroy();
     }
+
+
 }
