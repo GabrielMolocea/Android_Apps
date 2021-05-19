3bd1853cc7b0064811ea77a3fda0e2c15a44f369
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-05-19 16:32:21 +0300

    Decoder JSON

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/DirectionJSONParser.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/DirectionJSONParser.java
@@ -55,5 +55,37 @@ public class DirectionJSONParser {
     }
 
     private List decodePoly(String encoder) {
+
+        List poly = new ArrayList();
+        int index = 0, len = encoder.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoder.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~ (result >> 1) : (result >> 1));
+            lat = dlat;
+
+            shift = 0;
+            result = 0;
+
+            do {
+                b = encoder.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
     }
 }
