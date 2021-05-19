6f0d3520ee6162530d102710355b58fe336854e9
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-05-19 16:18:04 +0300

    Added decdePoly

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/DirectionJSONParser.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/DirectionJSONParser.java
@@ -1,6 +1,9 @@
 package com.gabriel.taxifee;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -20,8 +23,37 @@ public class DirectionJSONParser {
             jRoutes = jsonObject.getJSONArray("routes");
 
             // Traversing all routes
-            for (int i = 0; )
-        }
+            for (int i = 0; i < jRoutes.length(); i++) {
+                jLegs = ((JSONObject) jRoutes.get(i)).getJSONArray("legs");
+                List path = new ArrayList<HashMap<String,String>>();
 
+                // Traversing all legs
+                for (int j = 0; j < jLegs.length(); j++) {
+                    jSteps = ((JSONObject) jRoutes.get(i)).getJSONArray("steps");
+
+                    // Traversing all legs
+                    for (int k = 0; k < jSteps.length(); k++) {
+                        String polyline = "";
+                        polyline = (String) ((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+                        List list = decodePoly(polyline);
+
+                        // Traversing all legs
+                        for (int l = 0; l < list.size(); l++) {
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("lat", Double.toString(((LatLng)list.get(l)).latitude));
+                            hashMap.put("lnl", Double.toString(((LatLng)list.get(l)).longitude));
+                            path.add(hashMap);
+                        }
+                    }
+                    routes.add(path);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return routes;
+    }
+
+    private List decodePoly(String encoder) {
     }
 }
