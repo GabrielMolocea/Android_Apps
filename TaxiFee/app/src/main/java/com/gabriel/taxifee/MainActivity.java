a226ac265b53f51467054f87684a2c170761fa8a
GabrielMolocea <gabrielgabriel1235@gmail.com>
2021-05-24 10:15:12 +0300

    A method to download json data from url

--- TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
+++ TaxiFee/app/src/main/java/com/gabriel/taxifee/MainActivity.java
@@ -31,6 +31,12 @@ import com.google.android.gms.tasks.Task;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -92,7 +98,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     private class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String doInBackground(String... url) {
             String data = "";
 
             try {
@@ -190,6 +196,42 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         return url;
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "" ;
+        InputStream inputStream = null;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL(strUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.connect();
+
+            inputStream = urlConnection.getInputStream();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String line = "";
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+            data = stringBuffer.toString();
+
+            bufferedReader.close();
+
+            Log.d("data", data);
+        } catch (Exception e) {
+            Log.d("Exception ", e.toString());
+        } finally {
+            inputStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
     }
 
     @Override
